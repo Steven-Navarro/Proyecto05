ea0fa9a6b673a8892e31c59e153d8e11f4271f8f
Steven <stevennavarrop@gmail.com>
2023-11-28 12:29:56 -0500

    Agregando DTO

--- MicroserviceCard/src/main/java/com/bbva/MicroserviceCard/DTO/CardDTO.java
+++ MicroserviceCard/src/main/java/com/bbva/MicroserviceCard/DTO/CardDTO.java
@@ -1,4 +1,31 @@
 package com.bbva.MicroserviceCard.DTO;
 
+import com.bbva.MicroserviceCard.entity.CardType;
+import com.bbva.MicroserviceCard.entity.Customer;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardDTO {
+
+    private Integer cardID;
+
+    private String numeroTarjeta;
+
+    private CardType tipo;
+
+    private BigDecimal saldoDisponible;
+
+    private Customer customer;
 }
--- MicroserviceEstablishment/src/main/java/com/bbva/MicroserviceEstablishment/DTO/EstablishmentDTO.java
+++ MicroserviceEstablishment/src/main/java/com/bbva/MicroserviceEstablishment/DTO/EstablishmentDTO.java
@@ -1,4 +1,19 @@
 package com.bbva.MicroserviceEstablishment.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EstablishmentDTO {
+
+    private Integer establishmentID;
+
+    private String nombre;
+
+    private String direccion;
 }
